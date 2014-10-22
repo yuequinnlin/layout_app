e3c930ff4592ca97b9767a5b6578e1a8d49066a3
Quinn <yuequinnlin@gmail.com>
2014-10-22 14:51:40 -0500

    Update 2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Autofeed App"
   end
 
   test "should get login" do
     get :login
     assert_response :success
+    assert_select "title", "Login | Login page"
   end
 
 end
